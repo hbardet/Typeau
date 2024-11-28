a59bc1ce7be752a97fcc2f4f8725876aa88aa999
BARDET Hugo <hugo.bardet@epitech.eu>
2024-11-28 11:03:22 +0100

    [Features][+] = add music

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import WaveContainer from "./WaveContainer";
 import CustomCarousel from "./Carousel";
+import WaveContainer from "./WaveContainer";
 
 function App() {
   return (
     <div style={{ width: "100%", height: "100vh", backgroundColor: "#1143bf" }}>
+      <audio id="audio" loop autoPlay>
+        <source
+          src={process.env.PUBLIC_URL + "/background-music.mp3"}
+          type="audio/mpeg"
+        />
+      </audio>
       <CustomCarousel svgSize="100%" />
       <WaveContainer baseColor="#2480a7" waveCount={20} />
     </div>
